e161e2706ad15056bbe6281f4c92cc849df42e77
Retse_dev <stergaldeano@gmail.com>
2024-01-25 18:28:45 +0100

    design improvement

--- js/code.js
+++ js/code.js
@@ -3,103 +3,99 @@ const apiURL= 'https://www.dnd5eapi.co/api';
 
 //RELLENAMOS TEMPLATE CON LOS DATOS DEL JSON
 
-const classPanel = document.querySelector('#class-panel');
-const template = document.querySelector('#box-classes-template').content;
-const fragment = document.createDocumentFragment();
+/**************************CLASES***************************************** */
+    const classPanel = document.querySelector('#class-panel');
+    const template = document.querySelector('#box-classes-template').content;
+    const fragment = document.createDocumentFragment();
 
-async function fetchClassFromFile(){
-    let responseClass = await fetch('../data/character-class.json');
-    let classData = await responseClass.json();
-    
-    classData.forEach(item =>{
-        template.querySelector('p').textContent = item.class_name;
-        template.querySelector('img').src = item.imgURL;
-        template.querySelector('img').alt = item.class_name;
-        const box = template.cloneNode(true);
-        box.querySelector("div");
-        box.querySelector('div').addEventListener("click",showClassDetail);
-        fragment.appendChild(box);
-    });
+    async function fetchClassFromFile(){
+        let responseClass = await fetch('../data/character-class.json');
+        let classData = await responseClass.json();
 
+        classData.forEach(item =>{
+            template.querySelector('p').textContent = item.class_name;
+            template.querySelector('img').src = item.imgURL;
+            template.querySelector('img').alt = item.class_name;
+            const box = template.cloneNode(true);
+            box.querySelector("div");
+            box.querySelector('div').addEventListener("click",showClassDetail);
+            fragment.appendChild(box);
+        });
+ 
     classPanel.appendChild(fragment);
 }
 window.addEventListener('DOMContentLoaded',  fetchClassFromFile);
 
-//TEMPLATE INFO
-const racePanel = document.querySelector('#race-subrace-panel');
-const templateRace = document.querySelector('#box-races-template').content;
-const fragmentRace = document.createDocumentFragment();
+/**************************RACE***************************************** */
+    const racePanel = document.querySelector('#race-subrace-panel');
+    const raceTemplate = document.querySelector('#box-races-template').content;
+    const raceFragment = document.createDocumentFragment();
 
-/******************************LLAMADA API CON ENDPOINT *******************/
-async function fetchData(endpoint){
-    const response = await fetch (apiURL +'/'+endpoint);
-    if (!response.ok) return []
-    const data = await response.json();
-    return data;
+    async function fetchData(endpoint){
+        const response = await fetch (apiURL +'/'+endpoint);
+        if (!response.ok) return []
+        const data = await response.json();
+        return data;
 }
+    async function fetchToTemplate() {
+    let raceResponse = await fetch('../data/character-race.json');
+    let raceData = await raceResponse.json();
 
-async function fetchRacesToTemplate() {
-let responseRace = await fetch('../data/character-race.json');
-let raceData = await responseRace.json();
-
-//---------------------JSON RACE IMAGE-------------------------------------
-    raceData.forEach(item => {
-        
-        templateRace.querySelector('img').src = item.imgURL;
-        templateRace.querySelector('img').alt = item.index;
-        templateRace.querySelector('p').textContent = item.name;
-
-        const box = templateRace.cloneNode(true);
-        fragmentRace.appendChild(box);
-        
-    });racePanel.appendChild(fragmentRace);
+    raceData.forEach(item => {  
+        raceTemplate.querySelector('img').src = item.imgURL;
+        raceTemplate.querySelector('img').alt = item.index;
+        raceTemplate.querySelector('p').textContent = item.name;
+        const box = raceTemplate.cloneNode(true);
+        raceFragment.appendChild(box);  
+    });
+        racePanel.appendChild(raceFragment);
     
-//---------------------API RACE NAME-------------------------------------
-    // let dndRaces = [] 
-    // fetchData('races').then((item) => {
-    //     dndRaces = item.results;
-    
-    //     dndRaces.forEach(item=>{
-    //         templateRace.querySelector('p').textContent = item.name;
-    //         const boxRace = templateRace.cloneNode(true);
-    //         fragmentRace.appendChild(boxRace);
-    //     });
-    //     racePanel.appendChild(fragmentRace);
-    // });
-
-
-//************** API ALIGNMENT*************/
-    const templateAlignment = document.querySelector('#box-alignments-template').content; 
+/**************************ALIGNMENT***************************************** */
+    const alignmentTemplate = document.querySelector('#box-alignments-template').content; 
     const alignmentPanel = document.querySelector('#alignment-panel');
-    const fragmentAlignment = document.createDocumentFragment();
-            let responseAlignment = await fetch('../data/character-alignments.json');
-        let alignmentData = await responseAlignment.json();
+    const alignmentFragment = document.createDocumentFragment();
+
+    let alignmentResponse = await fetch('../data/character-alignments.json');
+    let alignmentData = await alignmentResponse.json();
     let dndAlignments = [];
 
     fetchData('alignments').then((item)=>{
         dndAlignments = item.results;
         dndAlignments.forEach(item=>{
-            templateAlignment.querySelector('p').textContent = item.name;
+            alignmentTemplate.querySelector('p').textContent = item.name;
             alignmentData.forEach(element =>{
                 if(element.id === item.index){
-                    templateAlignment.querySelector('img').src = element.imgURL;
+                    alignmentTemplate.querySelector('img').src = element.imgURL;
                 }
             });
-            const boxAlignment = templateAlignment.cloneNode(true);
-            fragmentAlignment.appendChild(boxAlignment); 
+            const alignmentBox = alignmentTemplate.cloneNode(true);
+            alignmentFragment.appendChild(alignmentBox); 
         });
-        alignmentPanel.appendChild(fragmentAlignment);
+        alignmentPanel.appendChild(alignmentFragment);
+    });
 
-});
+/**************************SKILLS***************************************** */
+
+    const skillsTemplate = document.querySelector('#box-skills-template').content;
+    const skillsPanel = document.querySelector('#ability-panel');
+    const skillsFragment = document.createDocumentFragment();
+    let skillsDnD = [];
+
+    fetchData('ability-scores').then((item) => {
+        skillsDnD = item.results;
+
+        skillsDnD.forEach(item => {
+            skillsTemplate.querySelector('span').textContent = item.name;
+            const skillsBox = skillsTemplate.cloneNode(true);
+            skillsFragment.appendChild(skillsBox);
+            });
+        skillsPanel.appendChild(skillsFragment);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchRacesToTemplate();
+    fetchToTemplate();
 });
-
-
-
-
 
 function changeCentralPanel(panelId) {
     // Ocultar todos los paneles centrales
@@ -107,38 +103,31 @@ function changeCentralPanel(panelId) {
     document.getElementById('race-subrace-panel').style.display = 'none';
     document.getElementById('class-panel').style.display = 'none';
     document.getElementById('alignment-panel').style.display = 'none';
-    document.getElementById('backstory-panel').style.display = 'none';
+    document.getElementById('avatar-panel').style.display = 'none';
     document.getElementById('ability-panel').style.display = 'none';
 
     // Mostrar el panel central correspondiente al ID
-    if(panelId==="class"||panelId==="race-subrace"){
+    if(panelId==="class"||panelId==="race-subrace" || panelId==="alignment"){
         document.getElementById(panelId + '-panel').style.display = 'flex';
+
     }else{
         document.getElementById(panelId + '-panel').style.display = 'block';
-        document.getElementById(panelId + '-panel').style.height = '80vh';
-        
-    }
-    
+       // document.getElementById(panelId + '-panel').style.width ='80vh';
+
+    }   
 }
 
 function showClassDetail(){
     this.style.background = 'black';
 }
 
+function sumar() {
+    var input = document.getElementById('numeroInput');
+    input.value = parseInt(input.value) + 1;
+}
 
-// function showInfoInterface(){
-    
-//     const container = document.querySelector('.list-classes');    
-
-//     const classList = document.querySelector('#class-list');
-    
-//     for (const dandclass of dandclasses){
-//         let itemList = document.createElement('li');
-//         let doneIcon = document.createElement('img');
-        
-//         itemList.classList.add('list-group-class');
-//         itemList.innerText = dandclass.name;
-//         classList.appendChild(itemList);
-//     }
-// }
+function restar() {
+    var input = document.getElementById('numeroInput');
+    input.value = parseInt(input.value) - 1;
+}
 
